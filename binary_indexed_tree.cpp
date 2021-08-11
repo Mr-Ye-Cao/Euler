f753ee2e5ae8f5e58a2e934e0d66f8455fec1fe1
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2021-08-10 20:13:18 -0500

    Update binary_indexed_tree.cpp

--- binary_indexed_tree.cpp
+++ binary_indexed_tree.cpp
@@ -6,19 +6,43 @@ using namespace std;
 void solve(int arr[],int N){
     int bit[N+1];
 
-    for(int i=0;i<N+1;i++)
-        if(i==0)
-            bit[i]=0;
-        else
-            bit[i+1]=arr[i];
+    bit[0]=0;
+
+    for(int i=1;i<N+1;i++)
+        bit[i]=arr[i-1];
+       
 
     for(int i=1;i<N+1;i++){
         int pi=(i+(i&-i));
-        while((i+i&-i)<N+1){
+        while(pi<N+1){
             bit[pi]+=arr[i-1];
-            pi=(pi+(pi&-pi));
+            pi+=(pi&-pi);
         }
     }
+
+    for(int i=0;i<N+1;i++)
+        printf("%d ",bit[i]);
+
+    while(true){
+        int l,r;
+        scanf("%d %d",&l,&r);
+
+        l--;
+        int lr=0,rr=0;
+        while(l>=1){
+            lr+=bit[l];
+            l-=l&-l;
+        }
+        while(r>=1){
+            rr+=bit[r];
+            r-=r&-r;
+        }
+
+        printf("%d\n",rr-lr);
+
+        // sum(l,r)=sum(1,r)-sum(1,l-1)
+    }
+
 }
 
 int main(){
@@ -33,7 +57,12 @@ int main(){
 
     solve(arr,N);
 
-    // printf("%d\n",1+(1&-1));
+//     while(true){
+//         int n;
+//         scanf("%d",&n);
+//         printf("%d",(n-(n&-n)));
+//     }
+    // 7->6->4->0   for computing sum(1,k) it is broken into several pieces and sum every piece together
 
     return 0;
 }

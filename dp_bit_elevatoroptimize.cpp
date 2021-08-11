7611c00e9f2d04dd2459c1492bc0d019574673fc
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2021-08-11 11:30:57 -0500

    Create dp_bit_elevatoroptimize.cpp

---
+++ dp_bit_elevatoroptimize.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int arr[],int N,int W){
+    pair<int,int> dp[1<<N];
+
+    dp[0]={1,0};
+
+    for(int i=1;i<(1<<N);i++){
+        dp[i]={N+1,0};
+
+        for(int j=0;j<N;j++){
+            if(i&(1<<j)){
+                pair<int,int> option=dp[i^(1<<j)];
+                if(option.second+arr[j]<=W){
+                    option.second+=arr[j];
+                }else{
+                    option.first++;
+                    option.second=arr[j];
+                }
+
+                dp[i]=min(dp[i],option);
+            }
+        }
+    }
+
+    printf("%d\n",dp[(1<<N)-1].first);
+}
+
+int main(){
+    int N,W;
+
+    scanf("%d %d",&N,&W);
+
+    int arr[N];
+
+    for(int i=0;i<N;i++)
+        scanf("%d",arr+i);
+
+    solve(arr,N,W);
+
+    return 0;
+}

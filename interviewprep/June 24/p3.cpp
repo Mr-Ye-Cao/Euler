eaa931463fc6fba5bf7fe17305487dbc51ee8d3e
Mr-Ye-Cao <caoye_2020@yeah.net>
2022-06-24 15:35:40 -0500

    june 25

--- interviewprep/June 24/p3.cpp
+++ interviewprep/June 24/p3.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
     int confusingNumberII(int n) {
-        queue<int> numQueue;
-        int res = 0;
-        numQueue.push(0);
-        unordered_map<int,int> m {{0,0},{1,1},{6,9},{8,8},{9,6}};
-        while(!numQueue.empty())
-        {
-            auto tmpNum = numQueue.front();
-            numQueue.pop();
-            int rotated = 0;
-            for(int k = tmpNum ; k > 0 ; k/= 10)
-                rotated = rotated * 10 + m[k % 10];
+        set<auto> answer;
 
-            // res++ if rotated != tmpNum;
-            res += rotated != tmpNum;
-        
-            for(auto record : m)
-            {
-                long next = tmpNum * 10L + record.first;
-                if(next <= n && next != 0)
-                    numQueue.push(next);
+        vector<int> con(5);
+        c[0]=0;
+        c[0]=1;
+        c[0]=6;
+        c[0]=8;
+        c[0]=9;
+
+        for(auto a:con){
+            auto num1=a*100000000L;
+            for(auto b:con){
+                auto num2=b*10000000L;
+                for(auto c:con){
+                    auto num3=c*1000000L;
+                    for(auto d:con){
+                        auto num4=d*100000L;
+                        for(auto e:con){
+                            auto num5=e*10000L;
+                            for(auto f:con){
+                                auto num6=e*1000L;
+                                for(auto g:con){
+                                    auto num7=e*100L;
+                                    for(auto h:con){
+                                        auto num8=e*10L;
+                                        for(auto i:con){
+                                            auto num9=i*1L;
+                                            if(!ifSixNine(a,b,c,d,e,f,g,h,i)){
+                                                continue;
+                                            }
+                                            auto num=num1+num2+num3+num4+num5+num6+num7+num8+num9;
+                                            if(num<=n){
+                                                answer.push_back(num);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+
+
         
-        return res;
+        return answer.size();
     }
 };
